a969643ea3498e88c0425d232700a20a29e96b39
Bart Dorsey <bart@bartdorsey.com>
2020-10-27 17:11:12 -0500

    Updating to include more demos and some react custom hooks demos

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import React from 'react';
-
+import React from "react";
+import {
+  CssBaseline,
+} from "@material-ui/core";
+import NavBar from './NavBar';
+import Theme from './Theme';
+import Home from './Home';
 function App() {
   return (
-    <h1>Hello world!</h1>
+    <>
+      <CssBaseline />
+      <Theme>
+        <NavBar />
+        <Home/>
+      </Theme>
+    </>
   );
 }
 
